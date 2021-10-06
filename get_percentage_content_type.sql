ffb9e25217f8aa0b95858d5a0aebe0c1482eafd5
Masatora Atarashi <atarashi.masatora@gmail.com>
2021-10-06 23:06:17 +0900

    Update get_percentage_content_type.sql

--- get_percentage_content_type.sql
+++ get_percentage_content_type.sql
@@ -1,1 +1,12 @@
--- コンテンツの種類(Web, Video, PDF)の割合を取得する
+-- コンテンツの種類(web, video, pdf, file)の割合を取得する
+
+SELECT
+    (100.0 * web / (SELECT count(*) FROM content)) as web,
+    (100.0 * video / (SELECT count(*) FROM content)) as video,
+    (100.0 * pdf / (SELECT count(*) FROM content)) as pdf,
+    (100.0 * file / (SELECT count(*) FROM content)) as file
+FROM
+    (SELECT count(*) as web FROM content WHERE content_type = 'web') as web,
+    (SELECT count(*) as video FROM content WHERE content_type = 'video') as video,
+    (SELECT count(*) as pdf FROM content WHERE content_type = 'pdf') as pdf,
+    (SELECT count(*) as file FROM content WHERE content_type = 'file') as file
