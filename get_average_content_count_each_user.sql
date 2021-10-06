f124d0af8bd5d018e3d1159230276d004f41cb6d
Masatora Atarashi <atarashi.masatora@gmail.com>
2021-10-06 22:26:18 +0900

    Update get_average_content_count_each_user.sql

--- get_average_content_count_each_user.sql
+++ get_average_content_count_each_user.sql
@@ -1,1 +1,10 @@
 -- 一人あたりの平均コンテンツ保存数を取得する
+
+SELECT AVG(content_count)
+FROM (
+         SELECT u.id, COUNT(c.id) as content_count
+         FROM users as u
+                  LEFT JOIN content as c ON u.id = c.user_id
+         GROUP BY u.id
+     ) as user_content_count
+;
